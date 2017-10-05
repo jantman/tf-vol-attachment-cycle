cd919e8d83713dc1f9ba1b8481a7cabf904d8329
Jason Antman <jason@jasonantman.com>
2017-10-04 20:51:03 -0400

    issue is much simpler than I thought

--- main.tf
+++ main.tf
@@ -15,16 +15,11 @@ provider "template" {
   version = "~> 0.1"
 }
 
-data "aws_subnet" "snet" {
-  id = "${var.subnet_id}"
-}
-
 # Note - in my actual use case, there are quite a few bits of data
 # pulled from terraform_remote_state into locals. I've added vars so
 # others can run this, but left the locals in place.
 locals {
   subnet_id        = "${var.subnet_id}"
-  az               = "${data.aws_subnet.snet.availability_zone}"
   common_tags      = {
     terraform_issue = "16237"
   }
@@ -57,7 +52,7 @@ resource "aws_instance" "ecs-instance" {
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "${path.module}/instance-destroy.sh '${aws_instance.ecs-instance.public_ip}' '${var.persistent_ebs_mountpoint}'"
+    command = "${path.module}/instance-destroy.sh '${aws_instance.ecs-instance.public_ip}'"
   }
 
 }
@@ -83,45 +78,8 @@ data "template_file" "user_data" {
 
   vars {
     cloudwatch_prefix          = ""
-    persistent_ebs_device_name = "${var.persistent_ebs_device_name}"
-    persistent_ebs_mountpoint  = "${var.persistent_ebs_mountpoint}"
     something_that_changes     = "${var.something_that_changes}"
   }
-}
-
-#
-# Volume Attachment
-#
-
-resource "aws_volume_attachment" "ecs-instance-persistent-vol-att" {
-  device_name = "${var.persistent_ebs_device_name}"
-  volume_id   = "${aws_ebs_volume.ecs-instance-persistent-vol.id}"
-  instance_id = "${aws_instance.ecs-instance.id}"
-}
-
-#
-# Persistent EBS Volume
-#
-
-resource "aws_ebs_volume" "ecs-instance-persistent-vol" {
-  availability_zone = "${local.az}"
-  type              = "standard"
-  size              = "1" # GB
-
-  tags = "${merge(
-    local.common_tags,
-    map(
-      "Name", "tf-issue-16237"
-    )
-  )}"
-}
-
-output "ebs_persistent_volume_id" {
-  value = "${aws_ebs_volume.ecs-instance-persistent-vol.id}"
-}
-
-output "ebs_persistent_volume_device_name" {
-  value = "${aws_ebs_volume.ecs-instance-persistent-vol.device_name}"
 }
 
 #
@@ -141,25 +99,13 @@ variable "subnet_id" {
 variable "ami_id" {
   description = "AMI ID to use for ECS host"
   type        = "string"
-  default     = "ami-1d668865" # us-west-2 amzn-ami-2017.03.f-amazon-ecs-optimized
+  default     = "ami-e689729e" # us-west-2 Amazon Linux 2017.09.0.20170930 HVM
 }
 
 variable "instance_type" {
   description = "EC2 Instance Type for ECS instance"
   type        = "string"
   default     = "t2.micro"
-}
-
-variable "persistent_ebs_mountpoint" {
-  description = "Mountpoint of persistent EBS volume"
-  type        = "string"
-  default     = "/persistent"
-}
-
-variable "persistent_ebs_device_name" {
-  description = "Device name of persistent EBS volume attachment"
-  type        = "string"
-  default     = "/dev/sdf"
 }
 
 variable "something_that_changes" {
